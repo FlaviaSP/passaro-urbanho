71e68e80e59a83a4254d5cd7a019281466ce0be7
FlaviaSP <flaviastefany332@gmail.com>
2024-06-17 21:14:27 -0300

    feat: criacao dos novos componentes e ajuste

---
+++ app-traduzir/src/app/painel/painel.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrl: './painel.component.css'
+})
+
+export class PainelComponent {
+  constructor(){}
+  
+  ngOnInit() {
+    
+  }
+}
---
+++ app-traduzir/src/app/topo/topo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrl: './topo.component.css'
+})
+export class TopoComponent {
+
+}
