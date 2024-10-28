2277a398aadcd038600d0a48a5935f5c5abb2e42
FlaviaSP <flaviastefany332@gmail.com>
2024-10-28 19:42:56 -0300

    feat: inicio

--- app-passaro/src/app/app.component.spec.ts
+++ app-passaro/src/app/app.component.spec.ts
@@ -14,16 +14,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'app-traduzir' title`, () => {
+  it(`should have the 'app-passaro' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-traduzir');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-traduzir');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-passaro');
   });
 });
